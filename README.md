ae8fcb100f9dbf2a6fc11d9ca7a2063cab7a0c29
Rashad Aliyev <aliyev.resad@gmail.com>
2021-04-23 14:15:49 +0400

    Just started WP configuration

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ Create a new theme: https://underscores.me/
 [How To Create Custom URL](https://webocreation.com/blog/steps-create-custom-url-work-locally-xampp-localhost-virtual-host/)
 
 ACF https://www.advancedcustomfields.com/resources/
+
+WordPress documentations:
+[Pages and Templates](https://developer.wordpress.org/themes/template-files-section/page-template-files/)
---
+++ wp/wp-content/themes/rangar-az/page-about.php
@@ -0,0 +1,25 @@
+<?php /* Template Name: About Template */ ?>
+
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package ranger_az
+ */
+
+get_header();
+$a = 5;
+var_dump($a);
+?>
+
+HELLO
+
+<?php
+get_footer();
