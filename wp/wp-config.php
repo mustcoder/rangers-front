c137eb2eb3da102c235c409e412f367b8e011310
ilyasrzazade <rzazadehmurad@gmail.com>
2021-04-27 23:30:08 +0400

    test5

--- wp/wp-config.php
+++ wp/wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'rangers' );
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'mysql' );
+define( 'DB_PASSWORD', '' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
--- wp/wp-content/themes/rangar-az-child/partials/layouts/navbar.php
+++ wp/wp-content/themes/rangar-az-child/partials/layouts/navbar.php
@@ -1,17 +1,17 @@
 <div class="row">
-    <div class="col-md-3 col-lg-6">
+    <div class="col-md-3 col-lg-5">
         <a href="http://rangers.az/">
-            <img src="https://www.inter.it/static/images/icons/soccerSchoolIcon@2x.png" alt="logo" height=100px class="float-left">
+            <img src="https://internationalfootball.academy/wp-content/themes/fcvacademy/assets/img/fcv-international-football-academy.png">
         </a>
     </div>
-    <div class="col-md-9 col-lg-6" id="uppermenu">
-        <div class="row">
-            <div class="col-xs-12">
+    <div class="col-md-9 col-lg-7" id="uppermenu">
+        
+            <div class="col-xs-12 ">
                 <p class="text-right text-danger font-weight-bold">
                     +1223212312312
                 </p>
             </div>
-        </div>
+        
         <div>
             <nav class="navbar navbar-expand-lg navbar-light bg-white text-uppercase">
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -32,7 +32,7 @@
                         <a href="#" class="nav-link text-dark">events</a>
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link text-dark">news</a>
+                        <a href="www\rangers-front\wp\wp-content\themes\rangar-az-child\partials\layouts\root-page.php" class="nav-link text-dark">news</a>
                     </li>
                     <li class="nav-item">
                         <a href="#" class="nav-link text-dark text-uppercase">how to join</a>
