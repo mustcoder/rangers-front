3c26714700d141d475e382a87ab461caf60c20c6
Rashad Aliyev <aliyev.resad@gmail.com>
2021-05-07 12:44:11 +0400

    Added theme and taxonomy

--- wp/wp-content/themes/rangar-az-child/partials/custom-posts/sidebar.php
+++ wp/wp-content/themes/rangar-az-child/partials/custom-posts/sidebar.php
@@ -62,29 +62,6 @@
 
 <!--Sidebar start-->
 <div class="sidebar">
-    <h3>ACADEMY LIFE</h3>
-    <p>Learn more about life student</p>
-    <ul>
-        <li><span><a href="#">Accomodation</a></span></li>
-        <li><span><a href="#">Student</a></span></li>
-        <li><span><a href="#">Activity</a></span></li>
-        <li><span><a href="#">Taster days</a></span></li>
-        <li><span><a href="#">Visa guide</a></span></li>       
-    </ul>
-    <h3>Football short courses</h3>            
-            <img 
-                src="/img/pexels-omar-ramadan-5886522.jpg" 
-                class="img-thumbnail border-0"
-            alt="foto">              
-        <p>
-            We offer short courses designed to give players an experience of what
-        </p>
-        <p class="read">
-            <a href="#">Read More</a>
-        </p>
-        <div class="ara"></div>
-    
-        <?php dynamic_sidebar('latest-sidebar') ?>
-
+    <?php dynamic_sidebar('inner-page-sidebar') ?>
 </div>
 <!--Sidebar End-->
